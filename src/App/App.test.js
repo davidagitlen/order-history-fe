3d4148ebba0ab861ca25c731897f4faca9ac6bfd
davidagitlen <davidagitlen@gmail.com>
2019-08-29 11:57:55 -0600

    Began adding additional async tests for fetches called in methods on App component, didn't quite finish
    :wq
    q

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import { getOrderHistory, postPurchase, deletePurchase } from '../util/apiCalls.js';
+
+jest.mock('../util/apiCalls.js');
 
 describe('App', () => {
   let wrapper; 
 
   beforeEach(() => {
+    getOrderHistory.mockImplementation(() => {
+      return Promise.resolve([{id: 1, image: 'broken-link.png', name: 'Blah', description: 'blah', price: 100}])
+    });
+    postPurchase.mockImplementation(() => {
+      return Promise.resolve({id: 2, image: 'broken-link.png', name: 'Two', description: 'two', price: 200})
+    });
     wrapper = shallow(<App />);
   })
   it('should match the snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should update state with data after mounting', () => {
+    shallow(<App />);
+    expect(getOrderHistory).toHaveBeenCalled();
+  });
+
+  it.skip('should add a purchase when addNewPurchase is called', () => {
+
+  })
 })
